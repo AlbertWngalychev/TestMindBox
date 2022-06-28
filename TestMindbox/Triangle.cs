2ac4694f5bb8b7f97ce58d507fb25c73f83d7af0
Альберт Енгалычев <albert150299@outlook.com>
2022-06-29 01:13:13 +0300

    add  test  static method Triangle.CreateWith2Side

--- TestMindbox/Triangle.cs
+++ TestMindbox/Triangle.cs
@@ -1,60 +1,89 @@
-﻿namespace TestMindbox
+﻿using System;
+using System.Data.Common;
+
+namespace TestMindbox
 {
-    public class Triangle : IShape
+    public class Triangle : IShape, IEquatable<Triangle>
     {
-        public double SideA { get; init; }
-        public double SideB { get; init; }
-        public double SideC { get; init; }
-        public double Square
+        private readonly double _sideA;
+        private readonly double _sideB;
+        private readonly double _sideC;
+
+        public double SideA => _sideA;
+        public double SideB => _sideB;
+        public double SideC => _sideC;
+        public double GetSquare()
+        {
+            double sp = GetPerimeter() / 2;
+            return Math.Sqrt(sp * (sp - SideA) * (sp - SideB) * (sp - SideC));
+        }
+        public double GetPerimeter()
+        {
+            return SideA + SideB + SideC;
+        }
+        public bool IsRectangular
         {
             get
             {
-                double sp = Perimeter / 2;
-                return Math.Sqrt(sp * (sp - SideA) * (sp - SideB) * (sp - SideC));
+                double sideA2 = Math.Pow(_sideA, 2);
+                double sideB2 = Math.Pow(_sideB, 2);
+                double sideC2 = Math.Pow(_sideC, 2);
+
+                return sideA2 + sideB2 == sideC2
+                    || sideB2 + sideC2 == sideA2
+                    || sideC2 + sideA2 == sideB2;
             }
         }
-        public double Perimeter => SideA + SideB + SideC;
-        public bool IsRectangular => Math.Pow(SideA, 2) + Math.Pow(SideB, 2) == Math.Pow(SideC, 2);
+
         public Triangle(double sideA, double sideB, double sideC)
         {
-            if (sideA <= 0) throw new Exception("Side A <= 0 or null");
-            if (sideB <= 0) throw new Exception("Side B <= 0 or null");
-            if (sideC <= 0) throw new Exception("Side C <= 0 or null");
-
-            if (!(sideA + sideB > sideC
-                && sideA + sideC > sideB
-                && sideB + sideC > sideA))
+            if (!canCreate(sideA, sideB, sideC))
             {
                 throw new Exception("Can't create a triangle");
             }
-
-            SideA = sideA;
-            SideB = sideB;
-            SideC = sideC;
+            _sideA = sideA;
+            _sideB = sideB;
+            _sideC = sideC;
         }
-        private Triangle() { }
-        public static Triangle CreateWith2Side(double sideA, double sideB, double angelBetween, bool radian = false)
+
+        static bool canCreate(double sideA, double sideB, double sideC)
         {
-            double cos = Math.Cos(radian ? angelBetween : angelBetween * Math.PI / 180);
-            double sideC = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2) - 2 * sideA * sideB * cos);
-
-            return new(sideA, sideB,sideC);
+            return sideA > 0
+                && sideB > 0
+                && sideC > 0
+                && sideA + sideB > sideC
+                && sideA + sideC > sideB
+                && sideB + sideC > sideA;
         }
-
+        public bool Equals(Triangle? other)
+            => other is not null 
+            && SideA == other.SideA
+            && SideB == other.SideB
+            && SideC == other.SideC;
         public override bool Equals(object? obj)
         {
             if ((obj == null) || !this.GetType().Equals(obj.GetType()))
             {
                 return false;
             }
-            Triangle tr = (Triangle)obj;
-            return (tr.SideA == SideA) && (tr.SideB == SideB) && (tr.SideC == SideC);
 
+            Triangle tr = (Triangle)obj;
+
+            return Equals(tr);
         }
 
+        public static bool operator ==(Triangle triangle1, Triangle triangle2)
+        {
+            return triangle1.Equals(triangle2);
+        }
+        public static bool operator !=(Triangle triangle1, Triangle triangle2)
+        {
+            return !triangle1.Equals(triangle2);
+        }
         public override int GetHashCode()
         {
             return HashCode.Combine(SideA, SideB, SideC);
         }
+
     }
 }
