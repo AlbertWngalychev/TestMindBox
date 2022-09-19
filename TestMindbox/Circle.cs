8db78bc84c93368eebace1a89670107edf5c8309
Альберт Енгалычев <albert150299@outlook.com>
2022-09-19 12:19:37 +0300

    add IEquatable<Circle>

--- TestMindbox/Circle.cs
+++ TestMindbox/Circle.cs
@@ -1,29 +1,49 @@
-﻿namespace TestMindbox
+﻿using TestMindbox.Interfaces;
+
+namespace TestMindbox
 {
-    public class Circle : IShape
+    public class Circle : IShape, IEquatable<Circle>
     {
+        private readonly double _radius;
+
         public const double PI = Math.PI;
-        public double Radius { get; init; }
-        public double Square => PI*Math.Pow(Radius,2);
-        public double Perimeter => 2*Radius*PI;
+        public double Radius => _radius;
+        public double GetSquare() => PI*Math.Pow(_radius,2);
+        public double GetPerimeter() => 2 * _radius * PI;
         public Circle(double radius)
         {
-            if (radius <= 0) throw new Exception("Radius <= 0");
-            Radius = radius;
+            if (radius <= 0)
+            {
+                throw new Exception("Radius <= 0");
+            }
+
+            _radius = radius;
         }
         public override bool Equals(object? obj)
         {
-            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            if (obj is null || !GetType().Equals(obj.GetType()))
             {
                 return false;
             }
-            Circle c = (Circle)obj;
-            return (c.Radius==Radius);
-        }
 
+            Circle circle = (Circle)obj;
+            return Equals(circle);
+        }
+        public static bool operator ==(Circle circle1, Circle circle2)
+        {
+            return circle1.Equals(circle2);
+        }
+        public static bool operator !=(Circle circle1, Circle circle2)
+        {
+            return !circle1.Equals(circle2);
+        }
         public override int GetHashCode()
         {
-            return HashCode.Combine(Radius);
+            return HashCode.Combine(_radius);
+        }
+        public bool Equals(Circle? other)
+        {
+            return other is not null && _radius == other.Radius;
         }
     }
 }
